5bd87bee44698444972289fb636455ec6bb4e9c1
James Spencer <hello@jmes.tech>
2018-05-18 15:50:06 +0100

    chore: now.sh config

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = next({
 })
 
 const handle = app.getRequestHandler()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.prepare().then(() => {
   createServer((req, res) => {
